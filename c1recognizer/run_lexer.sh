29a5a946d847971fa529e72e99b65d8a2c4c3a38
PB19111683 <angels@mail.ustc.edu.cn>
2023-09-16 20:32:59 +0800

    update antlr to 4.13.1

--- c1recognizer/run_lexer.sh
+++ c1recognizer/run_lexer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# change to your /full/path/to/antlr-4.7.2-complete.jar
-export CLASSPATH='.:/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH'
+# change to your /full/path/to/antlr-4.13.1-complete.jar
+export CLASSPATH='.:/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH'
 antlr4='java org.antlr.v4.Tool'
 grun='java org.antlr.v4.gui.TestRig'
 
@@ -23,10 +23,10 @@ elif [[ $1 == "cpp" ]]; then
     $antlr4 C1Lexer.g4 -Dlanguage=Cpp -o ../build
     cd ../build
     # uncomment below if you run this file on your machine
-    #g++ -std=c++11 ../test/lexer.cpp C1Lexer.cpp -I../include/antlr4-runtime -I. -L../Libs_for_c1r_ref/ -lantlr4-runtime -o c1lexer
+    #g++ -std=c++17 ../test/lexer.cpp C1Lexer.cpp -I../include/antlr4-runtime-4.13.1 -I. -L../Libs_for_c1r_ref/ -lantlr4-runtime -o c1lexer
     #LD_LIBRARY_PATH=../Libs_for_c1r_ref ./c1lexer
     # comment below if you run this file on your machine
-    g++ -std=c++11 ../test/lexer.cpp C1Lexer.cpp -I../include/antlr4-runtime -I. -lantlr4-runtime -o c1lexer
+    g++ -std=c++17 ../test/lexer.cpp C1Lexer.cpp -I../include/antlr4-runtime-4.13.1 -I. -lantlr4-runtime -o c1lexer
     ./c1lexer
 fi
 
